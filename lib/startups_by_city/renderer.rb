c875d29f828ee167e92067089ade825bb5ccba8b
Mike Schreifels <mike@schreifels.org>
2014-10-12 13:10:58 -0500

    Consolidate template files and highlight selected city in nav

--- lib/startups_by_city/renderer.rb
+++ lib/startups_by_city/renderer.rb
@@ -12,35 +12,30 @@ module StartupsByCity
         `rm -f #{File.join(BASE_PATH, 'output', '*')}`
         `cp #{File.join(BASE_PATH, 'assets', '*')} #{File.join(BASE_PATH, 'output')}`
 
-        puts
-        puts 'Rendering sidebar partial...'
-        sidebar_html = Haml::Engine.new(template_content('_sidebar')).
-            render(Object.new, collection: collection)
+        layout_engine = Haml::Engine.new(File.read(File.join(BASE_PATH, 'templates', 'layout.html.haml'))).
+            render_proc(Object.new, :collection, :country_name, :region_name, :city_name, :city_startups)
 
         puts
-        puts 'Rendering layout...'
-        layout_engine = Haml::Engine.new(template_content('layout')).
-            render_proc(Object.new, :sidebar_html, :content_html, :full_location_name)
+        puts 'Rendering index...'
         File.write(
           File.join(BASE_PATH, 'output', 'index.html'),
-          layout_engine.call(sidebar_html: sidebar_html)
+          layout_engine.call(collection: collection)
         )
 
         puts
         puts 'Rendering city pages...'
-        city_engine = Haml::Engine.new(template_content('city')).render_proc(Object.new, :location_name, :startups)
         collection.each do |country|
           country[:regions].each do |region|
             region[:cities].each do |city|
-              location_name = "#{city[:name]}, #{StartupsByCity::Regions.translate_region_code(country[:name], region[:name])}"
-              full_location_name = "#{city[:name]}, #{region[:name]}, #{country[:name]}"
-              puts "  Rendering page for #{full_location_name}"
+              puts "  Rendering page for #{city[:name]}, #{region[:name]}, #{country[:name]}"
               File.write(
                 File.join(BASE_PATH, 'output', city_path(country[:name], region[:name], city[:name])),
                 layout_engine.call(
-                  sidebar_html: sidebar_html,
-                  content_html: city_engine.call(location_name: location_name, startups: city[:startups]),
-                  full_location_name: full_location_name
+                  collection: collection,
+                  country_name: country[:name],
+                  region_name: region[:name],
+                  city_name: city[:name],
+                  city_startups: city[:startups]
                 )
               )
             end
@@ -53,16 +48,10 @@ module StartupsByCity
         int.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/) { "#{$1}," }
       end
 
-      def city_path(country, state, city)
+      def city_path(country_name, state_name, city_name)
         # transliterate ensures we only use ASCII characters in the URL
-        "#{I18n.transliterate(city)} #{I18n.transliterate(state)} #{I18n.transliterate(country)}".
+        "#{I18n.transliterate(city_name)} #{I18n.transliterate(state_name)} #{I18n.transliterate(country_name)}".
             downcase.gsub(/\W+/, '-') + '.html'
-      end
-
-      private
-
-      def template_content(template_name)
-        File.read(File.join(BASE_PATH, 'templates', template_name + '.html.haml'))
       end
     end
   end
