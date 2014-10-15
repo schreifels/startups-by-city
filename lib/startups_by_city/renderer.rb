cab74ade96761d0e22182e768a156286ce916248
Mike Schreifels <mike@schreifels.org>
2014-10-15 06:25:17 -0500

    Add pagination

--- lib/startups_by_city/renderer.rb
+++ lib/startups_by_city/renderer.rb
@@ -6,6 +6,8 @@ I18n.config.enforce_available_locales = true
 module StartupsByCity
   module Renderer
     class << self
+      STARTUPS_PER_PAGE = 250
+
       def render(collection, google_analytics)
         puts
         puts 'Preparing output directory...'
@@ -14,7 +16,9 @@ module StartupsByCity
         `cp #{File.join(BASE_PATH, 'assets', '*')} #{File.join(BASE_PATH, 'output')}`
 
         layout_engine = Haml::Engine.new(File.read(File.join(BASE_PATH, 'templates', 'layout.html.haml'))).
-            render_proc(Object.new, :google_analytics, :collection, :country_name, :region_name, :city_name, :city_startups)
+            render_proc(Object.new, :google_analytics, :collection,
+                :country_name, :region_name, :city_name, :city_startups,
+                :page, :total_pages)
 
         puts
         if google_analytics
@@ -36,17 +40,26 @@ module StartupsByCity
           country[:regions].each do |region|
             region[:cities].each do |city|
               puts "  Rendering page for #{city[:name]}, #{region[:name]}, #{country[:name]}"
-              File.write(
-                File.join(BASE_PATH, 'output', city_path(country[:name], region[:name], city[:name])),
-                layout_engine.call(
-                  google_analytics: google_analytics,
-                  collection: collection,
-                  country_name: country[:name],
-                  region_name: region[:name],
-                  city_name: city[:name],
-                  city_startups: city[:startups]
+
+              total_pages = (city[:startups_count].to_f / STARTUPS_PER_PAGE).ceil
+              (1..total_pages).each do |page|
+                end_index = page * STARTUPS_PER_PAGE - 1
+                start_index = end_index - STARTUPS_PER_PAGE + 1
+
+                File.write(
+                  File.join(BASE_PATH, 'output', city_path(country[:name], region[:name], city[:name], page)),
+                  layout_engine.call(
+                    google_analytics: google_analytics,
+                    collection: collection,
+                    country_name: country[:name],
+                    region_name: region[:name],
+                    city_name: city[:name],
+                    city_startups: city[:startups][start_index..end_index],
+                    page: page,
+                    total_pages: total_pages
+                  )
                 )
-              )
+              end
             end
           end
         end
@@ -58,11 +71,12 @@ module StartupsByCity
         @delimited_numbers[int] ||= int.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/) { "#{$1}," }
       end
 
-      def city_path(country_name, state_name, city_name)
+      def city_path(country_name, state_name, city_name, page=1)
         @city_paths ||= {}
         # transliterate ensures we only use ASCII characters in the URL
-        @city_paths[country_name + state_name + city_name] ||=
-            "#{I18n.transliterate(city_name)} #{I18n.transliterate(state_name)} #{I18n.transliterate(country_name)}".
+        @city_paths[country_name + state_name + city_name + page.to_s] ||=
+            ("#{I18n.transliterate(city_name)} #{I18n.transliterate(state_name)} " +
+            "#{I18n.transliterate(country_name)}#{' ' + page.to_s if page > 1}").
             downcase.gsub(/\W+/, '-') + '.html'
       end
     end
